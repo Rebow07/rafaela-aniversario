5b456693f5f83eef3ed2c6bbc6c33455c4938e48
Rebow07 <kellvin1107@hotmail.com>
2026-02-25 13:38:15 -0300

    resolve6

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import { Toaster } from "./components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
+import { TooltipProvider } from "./components/ui/tooltip";
 import { Route, Switch } from "wouter";
 import ErrorBoundary from "./components/ErrorBoundary";
 import { ThemeProvider } from "./contexts/ThemeContext";
